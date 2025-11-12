6e082294fabc91de1f8f7f1f0eb625133b8ca3e6
linh1999 <linhct020328@gmail.gmail.com>
2025-11-12 16:53:07 +0700

    add post

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { toast, ToastContainer } from "react-toastify";
 import { useParams, useRouter } from "next/navigation";
 import { useStateGeneral } from '@/useState/useStateGeneral';
@@ -15,7 +15,7 @@ const PostDetail: React.FC = () => {
     const { setLoading, setSelectNav
     } = useStateGeneral()
 
-    const { resPosts_Retrieve, setResPosts_Retrieve } = useState_ResPosts()
+    const { resPosts_Retrieve, setResPosts_Retrieve, resPosts_List, setResPosts_List } = useState_ResPosts()
 
     const getApiPostRetrieve = async (id: string, include: string) => {
         try {
@@ -30,10 +30,27 @@ const PostDetail: React.FC = () => {
         }
     }
 
+    const getApiPosts = async () => {
+        try {
+            setLoading(true);
+            const res = await ListAllPost()
+            setResPosts_List(res.data)
+        } catch (error: any) {
+            toast.error(`Posts: ` + error.response.error)
+        }
+        finally {
+            setLoading(false); // 👈 tắt loading sau khi có dữ liệu
+        }
+    }
+
     useEffect(() => {
         setSelectNav(null)
         getApiPostRetrieve(String(id), "post_category")
     }, [])
+
+    const filterPost = useMemo(() => {
+        return resPosts_List?.data.filter((r) => r.id !== id)
+    }, [resPosts_List, id])
 
     return (
         <>
@@ -51,18 +68,18 @@ const PostDetail: React.FC = () => {
                 </button>
             </div>
             {resPosts_Retrieve?.data &&
-                <div className="max-w-[1535px] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 px-5 py-5">
+                <div className="max-w-[1535px] mx-auto flex flex-col gap-5 px-5 py-5">
                     <div
-                        className="aspect-square relative rounded-md overflow-hidden relative group shadow-lg"
+                        className=" relative rounded-md overflow-hidden relative group shadow-lg w-full aspect-[16/6]"
                         rounded-md="fade-up"
                     >
                         <img
                             src={`${resPosts_Retrieve?.data.attributes.image_url}`}
                             alt={resPosts_Retrieve?.data.attributes.title}
-                            className="w-full h-full aspect-[1/1] object-cover transition-transform duration-700 group-hover:scale-105"
+                            className="aspect-[16/6] w-full object-cover transition-transform duration-700 group-hover:scale-105"
                         />
                         <span className='absolute top-3 left-3 rounded-full text-white px-4 py-1 bg-gradient-to-r from-emerald-500 to-green-700 text-white shadow-md backdrop-blur-md'>{resPosts_Retrieve?.data.attributes.post_category_title}</span>
-                        <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity"></div>
+                        <div className="absolute aspect-[16/6] inset-0 bg-gradient-to-t from-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity"></div>
                     </div>
                     <div className="flex flex-col gap-5">
                         <h1 className="text-4xl font-bold text-gray-900 leading-tight" >
@@ -87,6 +104,29 @@ const PostDetail: React.FC = () => {
                     </div>
                 </div >
             }
+            <div className='flex-grow gap-5 flex flex-col max-w-[1535px] mx-auto px-5 py-5'>
+                <div className="flex css-next items-center w-full transition-all duration-300 ease">
+                    <h3 className="text-xl uppercase font-semibold bg-clip-text tracking-wide">You may be interested</h3>
+                </div>
+                <div className="grid grid-cols-1 grid-cols-2 lg:grid-cols-3 gap-6">
+                    {filterPost?.map((res, id) => (
+                        <div className="relative group overflow-hidden rounded-md shadow-lg" key={id}
+                            onClick={() => {
+                                router.push(`/post/${res.id}`)
+                            }}
+                        >
+                            <img src={res.attributes.image_url!} alt={res.attributes.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
+                            <div className="absolute inset-0 bg-gradient-to-t from-black/50 via-transparent to-transparent opacity-70 group-hover:opacity-90 transition-opacity"></div>
+                            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black/70 via-transparent to-transparent rounded-b-md">
+                                <h3 className="text-xl text-white font-semibold mt-1">{res.attributes.title}</h3>
+                                <p className="text-white/70 text-sm mt-1">
+                                    {res.attributes.author_name} | Published: {new Date(res.attributes.published_at).toLocaleDateString()}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
             <ToastContainer position="top-right" autoClose={3000} />
         </>
     )
